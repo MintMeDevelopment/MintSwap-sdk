c726b0297dd15f6cfd7ed2690f018d216222cdbf
Ben Token Finance <admin@bentoken.finance>
2021-08-15 21:51:26 -0700

    ddd

--- dist/sdk-v2.esm.js
+++ dist/sdk-v2.esm.js
@@ -368,7 +368,7 @@ function Currency(decimals, symbol, name) {
  * The only instance of the base class `Currency`.
  */
 
-Currency.ETHER = /*#__PURE__*/new Currency(18, 'BNB', 'Binance');
+Currency.ETHER = /*#__PURE__*/new Currency(18, 'BCH', 'Bitcoin Cash');
 var ETHER = Currency.ETHER;
 
 var _WETH;
