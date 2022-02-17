f7fbe3dd37d34d7c66673d7f2e81a74406c24ad0
Jones <86437208+prsstech@users.noreply.github.com>
2022-02-17 19:02:09 +0100

    Update currency.ts

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,7 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'BCH', 'Bitcoin Cash')
+  public static readonly ETHER: Currency = new Currency(18, 'MintMe', 'MintMe')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
